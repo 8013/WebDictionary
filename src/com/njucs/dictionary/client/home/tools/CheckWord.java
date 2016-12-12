50d0bad7e5ad6dd3c0bfd4f72c946d45f3ec68f3
zhe <616308070@qq.com>
2016-12-12 15:00:25 +0800

    完善查词规则，允许类似a lot of词组的查询

--- src/com/njucs/dictionary/client/home/tools/CheckWord.java
+++ src/com/njucs/dictionary/client/home/tools/CheckWord.java
@@ -7,11 +7,12 @@ import com.njucs.dictionary.client.common.Message;
 public class CheckWord {
 
 	public static boolean Pass(String word){
+		word=word.trim();
 		if(word.isEmpty()){
 			Message.Show("请输入英文单词");
 			return false;
 		}
-		else if(!Pattern.matches("\\w{0,30}", word)){
+		else if(!Pattern.matches("\\w+( \\w+)*", word)){
 			Message.Show("请输入正确的英文单词");
 			return false;
 		}
